017daac48e81d4c0e612992309978fd73b29ccb3
Guanghao Liu <guanghao.liu@ecitele.com>
2023-12-26 09:50:44 +0800

    1226

---
+++ stack_1226.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+void LineEdit()
+{
+	stack<char> s;
+	char ch = getchar();
+	while (ch != EOF)
+	{
+		while (ch != EOF && ch != '\n')
+		{
+			switch (ch) 
+			{
+			case '#':
+				if (!s.empty())
+					s.pop();
+				break;
+			case '@':
+				while (!s.empty())
+					s.pop();
+				break;
+			default:
+				s.push(ch);
+				break;
+			}
+			ch = getchar();
+		}
+
+		string str;
+		auto it = str.begin();
+		while (!s.empty())
+		{
+			//it always point to fist element position
+			str.insert(it, s.top());
+			s.pop();
+		}
+		cout << str << "\n";
+		if (ch != EOF)
+			ch = getchar();
+	}
+}
+
+int main()
+{
+	LineEdit();
+	return 0;
+}
